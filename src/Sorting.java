63a0376bff39da7fdcb77b0259270c548f13331f
dyla1920 <dyla1920@ISS147D8-14.scdsb.pri>
2014-12-02 09:19:09 -0500

    Sorting

---
+++ src/Sorting.java
@@ -0,0 +1,252 @@
+
+import javax.swing.DefaultListModel;
+import javax.swing.JOptionPane;
+
+public class Sorting extends javax.swing.JFrame {
+
+    DefaultListModel model;
+    int nums[] = new int[50000];
+
+    public Sorting() {
+        initComponents();
+        model = new DefaultListModel();
+        //connect model to list
+        lstnums.setModel(model);
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jScrollPane1 = new javax.swing.JScrollPane();
+        lstnums = new javax.swing.JList();
+        btngenerate = new javax.swing.JButton();
+        btnbubble = new javax.swing.JButton();
+        btnselectiopn = new javax.swing.JButton();
+        btninsertion = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jScrollPane1.setViewportView(lstnums);
+
+        btngenerate.setText("Generate Numbers");
+        btngenerate.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btngenerateActionPerformed(evt);
+            }
+        });
+
+        btnbubble.setText("Bubble");
+        btnbubble.setToolTipText("");
+        btnbubble.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnbubbleActionPerformed(evt);
+            }
+        });
+
+        btnselectiopn.setText("Selection");
+        btnselectiopn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnselectiopnActionPerformed(evt);
+            }
+        });
+
+        btninsertion.setText("Insertion");
+        btninsertion.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btninsertionActionPerformed(evt);
+            }
+        });
+
+        jLabel1.setText("Sorting Methods");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(68, 68, 68)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(btngenerate, javax.swing.GroupLayout.DEFAULT_SIZE, 139, Short.MAX_VALUE)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE))
+                .addGap(46, 46, 46)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jLabel1, javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                        .addComponent(btninsertion, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(btnselectiopn, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(btnbubble, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                .addContainerGap(69, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap(21, Short.MAX_VALUE)
+                .addComponent(btngenerate)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 229, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(21, 21, 21))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(54, 54, 54)
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(btnbubble)
+                .addGap(18, 18, 18)
+                .addComponent(btnselectiopn)
+                .addGap(16, 16, 16)
+                .addComponent(btninsertion)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btngenerateActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btngenerateActionPerformed
+        model.clear();
+        for (int x = 0; x < nums.length; x++) {
+            nums[x] = (int) (Math.random() * 100000) + 1;
+            model.addElement(nums[x]);
+        }
+    }//GEN-LAST:event_btngenerateActionPerformed
+
+    private void btnbubbleActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnbubbleActionPerformed
+    model.clear();
+     long stime = System.nanoTime();
+    bubbleSort(nums);
+    long etime = System.nanoTime() - stime;
+    JOptionPane.showMessageDialog(this, etime);
+    //rebuildList
+    for(int x=0;x<nums.length;x++)
+        model.addElement(nums[x]);
+    }//GEN-LAST:event_btnbubbleActionPerformed
+
+    private void btnselectiopnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnselectiopnActionPerformed
+     model.clear();
+    selectionSort(nums);
+    //rebuildList
+    for(int x=0;x<nums.length;x++)
+        model.addElement(nums[x]);
+    }//GEN-LAST:event_btnselectiopnActionPerformed
+
+    private void btninsertionActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btninsertionActionPerformed
+     model.clear();
+    insertionSort(nums);
+    //rebuildList
+    for(int x=0;x<nums.length;x++)
+        model.addElement(nums[x]);
+    }//GEN-LAST:event_btninsertionActionPerformed
+    public static void bubbleSort(int[] a) {
+        int k = 0;
+        boolean exchangeMade = true;
+	// Make up to n - 1 passes through array, exit 
+        //early if no exchanges are made on previous pass
+
+        while ((k < a.length - 1) && exchangeMade) {
+            exchangeMade = false;
+            k++;
+            for (int j = 0; j < a.length - k; j++) {
+                if (a[j] > a[j + 1]) {
+                    swap(a, j, j + 1);
+                    exchangeMade = true;
+                }//end if
+            }
+        }//end for
+    }//end while
+
+//supporting swap method
+    public static void swap(int[] a, int x, int y) {
+        int temp = a[x];
+        a[x] = a[y];
+        a[y] = temp;
+    }
+public static void selectionSort(int[] a){
+   for (int i = 0; i < a.length - 1; i++){
+      int minIndex = findMinimum(a, i);
+      if (minIndex != i)
+	//if lowest is not already in place
+         swap(a, i, minIndex);
+   } //end for
+}  
+
+//supporting findMinimum method
+
+public static int findMinimum(int[] a, int first){
+   //first=where to start looking from
+   //assume first is also the smallest for now
+   int minIndex = first; 
+   for (int i = first + 1; i < a.length; i++)
+      if (a[i] < a[minIndex])
+         minIndex = i;
+   return minIndex;
+}
+public static void insertionSort(int a[]){
+	int itemToInsert, j;
+	boolean stillLooking;
+
+	//on the kth pass, pass item k upwards in list
+	//and insert it in its proper place amoung the
+	//first k entries in an array
+
+	for (int k=1; k<a.length; k++){
+	//move backwards through list, looking for
+	//the right place to insert a[k];
+		itemToInsert = a[k];
+		j=k-1;
+		stillLooking=true;
+		while(j >=0 && stillLooking){
+			if (itemToInsert < a[j]){
+				//move item higher
+				a[j+1] = a[j];
+				j--;
+			}else{
+				//we have found new home for a[k];
+				stillLooking=false;
+			}//end else// j+1 is where the item goes
+			a[j+1]=itemToInsert;
+		}//end while
+  	}//end for
+}//end method
+
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Sorting.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Sorting.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Sorting.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Sorting.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Sorting().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnbubble;
+    private javax.swing.JButton btngenerate;
+    private javax.swing.JButton btninsertion;
+    private javax.swing.JButton btnselectiopn;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JList lstnums;
+    // End of variables declaration//GEN-END:variables
+}
